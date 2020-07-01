030371faaf0bad2f667f8719c6f1acf0802d5d2c
Adam Adam <16069ac@kuranuicollege.school.nz>
2020-07-01 02:25:17 +0000

    dopdown, content, width responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
+window.onscroll = function() {myFunction2()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction2() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
 
+
+
+
+
+
+
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
 
 
