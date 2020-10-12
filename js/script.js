3d22567512b246c8add2e3a61dd79e0b4caaaac2
Adam Adam <16069ac@kuranuicollege.school.nz>
2020-10-12 23:16:41 +0000

    gui without objects

--- js/script.js
+++ js/script.js
@@ -10,12 +10,97 @@ toggleButton.addEventListener('click', () => {
 
 
 
+document.getElementById('container').onchange = function(){
+  var question1 = Number(document.getElementById("question1Input").value);
+
+  var question2 = Number(document.getElementById("question2Input").value);
+
+  var question3 = Number(document.getElementById("question3Input").value);
+
+  var question4 = Number(document.getElementById("question4Input").value);
+
+  var question5 = Number(document.getElementById("question5Input").value);
+
+  document.getElementById("question1Output").innerHTML = question1;
+
+  document.getElementById("question2Output").innerHTML = question2;
+
+  document.getElementById("question3Output").innerHTML = question3;
+
+  document.getElementById("question4Output").innerHTML = question4;
+
+  document.getElementById("question5Output").innerHTML = question5;
+
+  var results = 0;
+
+  if (question1 == 1){
+    results ++;
+  }
+  
+  if (question2 == 3){
+    results ++;
+  }
+  
+  if (question3 == 1){
+    results ++;
+  }
+  
+  if (question4 == 2){
+    results ++;
+  }
+  
+  if (question5 == 2){
+    results ++;
+  }
+  
+
+
+}
 
 
 
 
 
 
+ function submit(){
+  var question1 = Number(document.getElementById("question1Input").value);
+
+  var question2 = Number(document.getElementById("question2Input").value);
+
+  var question3 = Number(document.getElementById("question3Input").value);
+
+  var question4 = Number(document.getElementById("question4Input").value);
+
+  var question5 = Number(document.getElementById("question5Input").value);
+
+
+  var results = 0;
+
+  if (question1 == 1){
+    results ++;
+  }
+  
+  if (question2 == 3){
+    results ++;
+  }
+  
+  if (question3 == 1){
+    results ++;
+  }
+  
+  if (question4 == 2){
+    results ++;
+  }
+  
+  if (question5 == 2){
+    results ++;
+  }
+  
+  var grade = results * 20 + "%";
+  
+
+  document.getElementById("score").innerHTML = grade;
+}
 
 
 
