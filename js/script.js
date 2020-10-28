044094062f9773d04a7d1331adbafc2fb3ecd435
Adam Adam <16069ac@kuranuicollege.school.nz>
2020-10-28 01:32:29 +0000

    GUI with Object (works)

--- js/script.js
+++ js/script.js
@@ -102,7 +102,105 @@ document.getElementById('container').onchange = function(){
   document.getElementById("score").innerHTML = grade;
 }
 
+var hints = {
+  "Question1" : ["Algorithm", "Steps", "1"],
+  "Question2" : ["Time", "Seconds", "3"],
+  "Question3" : ["Time", "Years", "1"],
+  "Question4" : ["Something", "Quicken", "2"],
+  "Question5" : "5 seconds"
+}
 
+
+var q1 = 0;
+
+function hintsQuestion1(){
+if (q1 == 0){
+  var hint1 = hints["Question1"][0];
+  document.getElementById("hintsQ1Output").innerHTML = hint1; 
+}
+
+if (q1 == 1){
+  var hint2 = hints["Question1"][1];
+  document.getElementById("hintsQ1Output").innerHTML = hint2; 
+}
+if (q1 == 2){
+  var hint3 = hints["Question1"][2];
+  document.getElementById("hintsQ1Output").innerHTML = hint3; 
+}
+
+q1 ++;
+}
+
+var q2 = 0;
+
+function hintsQuestion2(){
+if (q2 == 0){
+  var hint1 = hints["Question2"][0];
+  document.getElementById("hintsQ2Output").innerHTML = hint1; 
+}
+
+if (q2 == 1){
+  var hint2 = hints["Question2"][1];
+  document.getElementById("hintsQ2Output").innerHTML = hint2; 
+}
+if (q2 == 2){
+  var hint3 = hints["Question2"][2];
+  document.getElementById("hintsQ2Output").innerHTML = hint3; 
+}
+
+q2 ++;
+}
+
+var q3 = 0;
+
+function hintsQuestion3(){
+if (q3 == 0){
+  var hint1 = hints["Question3"][0];
+  document.getElementById("hintsQ3Output").innerHTML = hint1; 
+}
+
+if (q3 == 1){
+  var hint2 = hints["Question3"][1];
+  document.getElementById("hintsQ3Output").innerHTML = hint2; 
+}
+if (q3 == 2){
+  var hint3 = hints["Question3"][2];
+  document.getElementById("hintsQ3Output").innerHTML = hint3; 
+}
+
+q3 ++;
+}
+
+var q4 = 0;
+
+function hintsQuestion4(){
+if (q4 == 0){
+  var hint1 = hints["Question4"][0];
+  document.getElementById("hintsQ4Output").innerHTML = hint1; 
+}
+
+if (q4 == 1){
+  var hint2 = hints["Question4"][1];
+  document.getElementById("hintsQ4Output").innerHTML = hint2; 
+}
+if (q4 == 2){
+  var hint3 = hints["Question4"][2];
+  document.getElementById("hintsQ4Output").innerHTML = hint3; 
+}
+
+q4 ++;
+}
+
+var q5 = 0;
+
+function hintsQuestion5(){
+if (q5 == 0){
+  var hint1 = hints["Question5"];
+  document.getElementById("hintsQ5Output").innerHTML = hint1; 
+}
+
+
+}
 
 
 
